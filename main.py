1d913d3b1a70be852886f8155a81e7558e25c7fd
VincentFokker <vincent.fokker@gmail.com>
2022-10-08 19:58:09 +0200

    new stuff

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.slimmemeterdata.slimmemeterlezer import SlimmeMeterLezer
+
+
+def main():
+    """Main function to run."""
+    lezer = SlimmeMeterLezer()
+    lezer.run()
+
+
+if __name__ == "__main__":
+    main()
